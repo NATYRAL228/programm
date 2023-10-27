1909effcd2844c44c669ae8e3f8afa679dd96c2d
NATYRAL228 <129774623+NATYRAL228@users.noreply.github.com>
2023-10-27 17:50:14 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame as pg
-
+a = pg.image.load('C:/Users/Ученик_8/Downloads/programm-main/pngwing.com.png')
+pga = pg.image.load('C:/Users/Ученик_8/Downloads/sergeyjpg.jpg')
 pg.init()
 font_name = pg.font.match_font('arial') #поиск шифта arial
 size = 18 #размер шрифта
@@ -18,7 +19,8 @@ def user_name(surf,text,x,y,size,color=(255,255,255)):
     text_rect = text_surface.get_rect()
     text_rect.midtop = (x, y)
     surf.blit(text_surface, text_rect)
-while 1:
+mainloop = True
+while mainloop:
   for i in pg.event.get():
     if i.type == pg.QUIT:
       exit()
@@ -26,10 +28,25 @@ while 1:
        if  i.key == pg.K_BACKSPACE:
            name = name[:-1]
        elif i.key == pg.K_RETURN:
-           print(name)
+           mainloop = False
        else:
            name += i.unicode
     win.fill((0,0,0))
+    win.blit(a,(0,0))
     draw_text(win, 'Введите имя:', W//2,H//2)
     draw_text(win,name,W//2,H//2 + 20)
-    pg.display.update(
+    pg.display.update()
+
+while 1:
+  for i in pg.event.get():
+    if i.type == pg.QUIT:
+      exit()
+  win.fill((0, 0, 0))
+  win.blit(pga, (0, 0))
+
+  for vertic in range(0,600,20):
+      pg.draw.line(win,(255,0,0),(0,vertic),(600,vertic))
+  for horiz in range(0, 600, 20):
+      pg.draw.line(win, (0, 255, 0), (horiz, 0), (horiz, 600))
+  draw_text(win,name,W//2, 20,color=(255,0,0))
+  pg.display.update()
